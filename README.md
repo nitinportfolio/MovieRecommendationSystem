96a7d7de117c149d4296930311eac4a99a949c71
Nitin <nitinportfolio.ai@gmail.com>
2023-09-06 10:40:40 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4633bfc392f41eb36af6001ce3382a4d303cace830890a48096919930f31b94d
+size 60
---
+++ app.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a5d96b0acace82c4029aec46705a1d6c8fe4fe09f602716e373ad2b5048f144e
+size 2199
